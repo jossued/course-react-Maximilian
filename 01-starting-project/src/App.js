01bed74620715cdf05d62b74d058f947e002a0df
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-05-21 11:47:49 -0500

    props

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
+      <Todo title='Learn React' />
+      <Todo title='Master React' />
+      <Todo title='Explore the full React' />
     </div>
   );
 }
--- 01-starting-project/src/components/Todo.js
+++ 01-starting-project/src/components/Todo.js
@@ -1,7 +1,7 @@
-function Todo() { // es necesario que los componentes empiecen con una Mayus
+function Todo(props) { // es necesario que los componentes empiecen con una Mayus
     return (
         <div className='card'>
-            <h2>title</h2>
+            <h2>{props.title}</h2>  {/* los corchetes son para abrir una expresión Js*/}
             <div className='actions '>
                 <button className='btn'>Delete</button>
             </div>
