4a3ea4584f73011f4a41cda4189868fc6460b4db
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-05-21 12:03:45 -0500

    function on button

--- 01-starting-project/src/components/Todo.js
+++ 01-starting-project/src/components/Todo.js
@@ -1,9 +1,12 @@
 function Todo(props) { // es necesario que los componentes empiecen con una Mayus
+    function deleteHandler() {
+        console.log(props.title);
+    }
     return (
         <div className='card'>
             <h2>{props.title}</h2>  {/* los corchetes son para abrir una expresión Js*/}
             <div className='actions '>
-                <button className='btn'>Delete</button>
+                <button className='btn' onClick={deleteHandler}>Delete</button>
             </div>
         </div>
     );
