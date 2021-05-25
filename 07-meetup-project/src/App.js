e58f46383f54fc38e0f69fdfa088cb1e2e286f69
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-05-25 10:14:31 -0500

    forma de crear links a los componentes

--- 07-meetup-project/src/App.js
+++ 07-meetup-project/src/App.js
@@ -2,12 +2,14 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetup";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation"
 
 function App() {
   // React usa el path y carga los componentes que coincidan con el path por ello cargaria siempre /
-  // para no cargar / se usa el switch y e
+  // para no cargar / se usa el switch y exact
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact>
           < AllMeetupsPage />
