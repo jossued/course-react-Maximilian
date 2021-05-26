13366c99de3398c0a3223b4c24757e38a237b9cc
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-05-26 11:28:04 -0500

    useContext para tener variables globales

--- 07-meetup-project/src/components/meetups/MeetupItem.js
+++ 07-meetup-project/src/components/meetups/MeetupItem.js
@@ -1,18 +1,39 @@
+import { useContext } from "react";
+
+import Card from '../ui/Card';
 import classes from './MeetupItem.module.css'
+import FavoritesContext from '../../store/favorites-context';
 
 function MeetupItem(props) {
+    const favoritesCtx = useContext(FavoritesContext);
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+    function toggleFavoriteStatusHandler() {
+        itemIsFavorite ? 
+        favoritesCtx.removeFavorite(props.id) : 
+        favoritesCtx.addFavorite({
+            id: props.id,
+            title: props.title,
+            descripcion: props.descripcion,
+            image: props.image,
+            address: props.address
+        })
+    }
     return <li className={classes.item}>
-        <div className={classes.image}>
-            <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-            <h3>{props.title}</h3>    
-            <address>{props.address}</address>
-            <p>{props.descripcion}</p>
-        </div>
-        <div className={classes.actions}>
-            <button>Favorites</button>
-        </div>
+        <Card>
+            <div className={classes.image}>
+                <img src={props.image} alt={props.title} />
+            </div>
+            <div className={classes.content}>
+                <h3>{props.title}</h3>
+                <address>{props.address}</address>
+                <p>{props.descripcion}</p>
+            </div>
+            <div className={classes.actions}>
+                <button onClick={toggleFavoriteStatusHandler}>
+                    {itemIsFavorite ? 'Remove from favorites' : 'Add to favorites'}
+                </button>
+            </div>
+        </Card>
     </li>
 }
 
