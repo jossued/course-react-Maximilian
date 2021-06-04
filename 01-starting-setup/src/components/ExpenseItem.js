e92df8bcc739e8aab37a2bbed5fc8ba8fdc5ad32
jossued <jossuedutan@gmail.com>
2021-06-04 14:52:55 -0500

    constants added dinamically to the page

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 5, 1);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 123.12;
+
     return <div className="expense-item">
-        <div>2021-06-01</div>
+        <div>{expenseDate.toDateString()}</div>
         <div className="expense-item__description">
-            <h2>Car insurance</h2>
-            <div className="expense-item__price">$123.22</div>
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">${expenseAmount}</div>
         </div>
     </div>;
 }
