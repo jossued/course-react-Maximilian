b2e4f8e0dab09434bc1f12221d0300733dbbce78
jossued <jossuedutan@gmail.com>
2021-06-04 15:22:39 -0500

    adding props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 5, 1);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 123.12;
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2021, 5, 1);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 123.12;
 
     return <div className="expense-item">
-        <div>{expenseDate.toDateString()}</div>
+        <div>{props.date.toDateString()}</div>
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">${expenseAmount}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     </div>;
 }
