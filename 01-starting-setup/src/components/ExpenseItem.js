949e20770302dcc564bcf953be690233d1d0abc5
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-06-04 14:42:46 -0500

    styles

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem(){
-    return <h2>EXPENSE ITEM!</h2>
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    return <div className="expense-item">
+        <div>2021-06-01</div>
+        <div className="expense-item__description">
+            <h2>Car insurance</h2>
+            <div className="expense-item__price">$123.22</div>
+        </div>
+    </div>;
 }
 
 export default ExpenseItem;
