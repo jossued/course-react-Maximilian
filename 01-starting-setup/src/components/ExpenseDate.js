87b53b7f4d2356a6d8a1e7406001fb7b653a0465
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-06-04 16:16:51 -0500

    adding styles to the calendar component

--- 01-starting-setup/src/components/ExpenseDate.js
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -1,12 +1,14 @@
+import './ExpenseDate.css';
+
 function ExpenseDate(props) {
     const month = props.date.toLocaleString('es-ES', { month: 'long' });
     const day = props.date.toLocaleString('es-ES', { day: '2-digit' });
     const year = props.date.getFullYear();
 
-    return <div>
-        <div>{year}</div>
-        <div>{month}</div>
-        <div>{day}</div>
+    return <div className="expense-date">
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__day">{day}</div>
     </div>
 }
 
